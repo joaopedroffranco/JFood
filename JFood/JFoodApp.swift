b40bafb59c3fbc2a4413ae5fb65f208b6a701ff9
João Pedro Fabiano Franco <joaopedroffranco.dev@gmail.com>
2025-11-25 13:42:17 +0100

    first setup

---
+++ JFood/JFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  JFoodApp.swift
+//  JFood
+//
+//  Created by Joao Pedro Fabiano Franco on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct JFoodApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
